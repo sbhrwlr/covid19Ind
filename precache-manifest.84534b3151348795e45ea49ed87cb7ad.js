f05dca41b7d612f42218f2fc487a4f1c7b403364
Rahul <sabharwalrahul2000@gmail.com>
2020-08-04 23:45:01 +0530

    Updates

---
+++ precache-manifest.84534b3151348795e45ea49ed87cb7ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1834f77731940ea588b5c24b2a4d534d",
+    "url": "/covid19Ind/index.html"
+  },
+  {
+    "revision": "cd2368b9ab5261b61e98",
+    "url": "/covid19Ind/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4591a153bd2370b68d7f",
+    "url": "/covid19Ind/static/css/main.3d5c82d9.chunk.css"
+  },
+  {
+    "revision": "cd2368b9ab5261b61e98",
+    "url": "/covid19Ind/static/js/2.141fef79.chunk.js"
+  },
+  {
+    "revision": "6485e28f1bc40c1e743bb4de5f286310",
+    "url": "/covid19Ind/static/js/2.141fef79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4591a153bd2370b68d7f",
+    "url": "/covid19Ind/static/js/main.1e2c4fe2.chunk.js"
+  },
+  {
+    "revision": "70cd11213e8237426f5d",
+    "url": "/covid19Ind/static/js/runtime-main.997c3936.js"
+  }
+]);
