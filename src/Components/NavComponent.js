229feb7f6fd0b478b7349ca29285d69ef042b8df
Rahul <sabharwalrahul2000@gmail.com>
2020-08-06 23:09:45 +0530

    final download

--- src/Components/NavComponent.js
+++ src/Components/NavComponent.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { Navbar, NavbarBrand,Nav, NavItem, NavLink} from 'reactstrap'
 import Lottie from 'react-lottie'
 import animdata from './logo.json'
-
+import logo from './virus.png'
+import { Button } from "reactstrap";
+import app from './app-debug.apk'
 class NavComponent extends Component {
 
     render(){
@@ -14,20 +16,22 @@ class NavComponent extends Component {
             }
         };
         return (
-            <Navbar  dark={true} style={{background: "black"}}>
+            <div>
+                        <Navbar  dark={true} style={{background: "black"}}>
             <div className="container-fluid ">
-            <NavbarBrand style={{color: "white"}}>Covid19Tracker.ind</NavbarBrand>
+            <NavbarBrand style={{color: "white"}}><img src={logo} style={{height: 30 , margin: "auto auto"}}/>Covid19 Tracker</NavbarBrand>
               <Lottie
+
                       options={defaultOptions}
                       height={100}
                       width={100}
                      style={{textAlign: "right"}}></Lottie>
             </div>
-            <Nav>
-            <NavItem>
-              <a href="https://srv-file14.gofile.io/download/klerlZ/app-debug.apk" download="Track">Android App</a>
-            </NavItem>            </Nav>
+          
           </Navbar>
+          
+          <Button color="primary" style={{marginTop: 5}}><a href={app} download style={{color: "white"}}>Android App</a></Button>
+            </div>
         )
     }
 
